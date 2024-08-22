e8ac1ebfcf279a562f9cbdac07409355cbc80094
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-08-22 14:43:02 -0300

    test

--- app.vue
+++ app.vue
@@ -6,7 +6,9 @@
       <input type="name" placeholder="ID" v-model="id" />
       <button @click="verify()">Validar</button>
       <p v-if="verified">{{ this.valid }}</p>
-      <p v-if="verified">Nombre: {{ this.nombre }} Apellido: {{ this.apellido }} ID: {{ this.new_id }}</p >
+      <p v-if="verified">Nombre: {{ this.nombre }}</p>
+      <p v-if="verified">Apellido: {{ this.apellido }}</p>
+      <p v-if="verified">ID: {{ this.id }}</p>
   </div>
 </template>
 <script>
@@ -24,14 +26,15 @@
       methods: {
           verify() {
               this.verified = true;
-              console.log("Nombre: " + this.nombre + " Apellido: " + this.apellido + " ID: " + this.id);
-              // id_sourcenumber should start after the 4th character and end before the - character
-              const numericPart = this.id.replace(/\D/g, ''); // Remove non-numeric characters
-              const id_sourcenumber = numericPart.slice(0, -1); // Remove the last digit
-              console.log("ID Source Number: " + id_sourcenumber);
-              let new_id = this.apellido.toUpperCase().slice(0, 2) + this.nombre.toUpperCase().slice(0, 2) + id_sourcenumber + '-' + this.createVerificationDigit(id_sourcenumber);
-              console.log("New ID: " + new_id);
+
+              const numericPart = this.id.replace(/\D/g, '');
+              const id_sourcenumber = numericPart.slice(0, -1); 
+
+              let verificationDigit = this.createVerificationDigit(id_sourcenumber);
+              let new_id = this.apellido.toUpperCase().slice(0, 2) + this.nombre.toUpperCase().slice(0, 2) + id_sourcenumber + '-' + verificationDigit;
+
               this.new_id = new_id;
+
               if(new_id === this.id){
                   this.valid = "OK";
               } else {
@@ -41,31 +44,28 @@
           createVerificationDigit(number) {
             let S = 0, O = 0, E = 0;
             let numLength = number.length;
-            let oddPosition = [];
-            let evenPosition = [];
-            let digits = [];
+            console.log("Take the numeric part of the ID without the verification digit: " + number);
             for (let i = 0; i < numLength; i++) {
               let digit = parseInt(number[i]);
               S += digit;
-              digits.push(digit);
               if (i % 2 === 0) {
                 O += digit;
-                oddPosition.push(digit);
               } else {
                 E += digit;
-                evenPosition.push(digit);
               }
             }
-            console.log("Digits: " + digits);
-            console.log("Odd Position Digits: " + oddPosition);
-            console.log("Even Position Digits: " + evenPosition);
-            console.log("S: " + S + " O: " + O + " E: " + E);
+            console.log("Sum all the numbers: " + S);
+            console.log("Sum all the numbers in an odd position: " + O);
+            console.log("Sum all the numbers in an even position: " + E);
+            console.log("Multiply the sum of the numbers in an odd position by 2: " + O * 2);
+            console.log("Multiply the sum of the numbers in an even position by 3: " + E * 3);
+            console.log("Sum all the by products: " + (O * 2) + (E * 3) + S + " = X");
             let X = (O * 2) + (E * 3) + S;
-            console.log("X: " + X);
-            let verificationDigit = X % 10;
-            console.log("Y / Verification Digit: " + verificationDigit);
+            let V = X % 10;
+            console.log("Divide X by 10 and get the remainder: " + V);
+            console.log("V is the verification digit: " + V);
 
-            return verificationDigit;
+            return V;
           }
       }
   }
