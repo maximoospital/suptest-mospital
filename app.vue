1dbbddd0de3f42e751764c3296627b459698f1df
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-08-22 13:54:37 -0300

    simplifying code

--- app.vue
+++ app.vue
@@ -24,47 +24,34 @@
               this.verified = true;
               console.log("Nombre: " + this.nombre + " Apellido: " + this.apellido + " ID: " + this.id);
               // id_sourcenumber should start after the 4th character and end before the - character
-              const start = 4;
-              const end = this.id.indexOf('-');
-              const id_sourcenumber = this.id.substring(start, end);
-              console.log("ID Source Number: " + id_sourcenumber);
-              let id_sourcenumber_sum = id_sourcenumber.toString().split('').map(Number).reduce(function (a, b){ return a + b; }, 0);
-              console.log("ID Source Number Sum: " + id_sourcenumber_sum);
-              let id_sourcenumber_sum_even = this.sumEven(id_sourcenumber);
-              console.log("ID Source Number Sum Even: " + id_sourcenumber_sum_even);
-              let id_sourcenumber_sum_odd = this.sumOdd(id_sourcenumber);
-              console.log("ID Source Number Sum Odd: " + id_sourcenumber_sum_odd);
-              let X = 3* id_sourcenumber_sum_even + 2*id_sourcenumber_sum_odd + id_sourcenumber_sum;
-              console.log("X: " + X);
-              let new_id_vd = X % 10;
-              console.log("New ID VD: " + new_id_vd);
-              let new_id = this.apellido.toUpperCase().slice(0, 2) + this.nombre.toUpperCase().slice(0, 2) + id_sourcenumber + '-' + new_id_vd;
-              console.log("ID: " + new_id);
+              const numericPart = this.id.replace(/\D/g, ''); // Remove non-numeric characters
+              const id_sourcenumber = numericPart.slice(0, -1); // Remove the last digit
+              let new_id = this.apellido.toUpperCase().slice(0, 2) + this.nombre.toUpperCase().slice(0, 2) + id_sourcenumber + '-' + this.createVerificationDigit(id_sourcenumber);
+              console.log("New ID: " + new_id);
               if(new_id === this.id){
                   this.valid = "OK";
               } else {
                   this.valid = "NOT VALID";
               }
           },
-          sumEven(number){
-              let sumNumber = 0;
-              let numLength = number.length;
-              for (let i = 0; i < numLength; i++) {
-                  if (i % 2 != 0) {
-                      sumNumber += parseInt(number[i]);
-                  }
+          createVerificationDigit(number) {
+            let S = 0, O = 0, E = 0;
+            let numLength = number.length;
+
+            for (let i = 0; i < numLength; i++) {
+              let digit = parseInt(number[i]);
+              S += digit;
+              if (i % 2 === 0) {
+                O += digit;
+              } else {
+                E += digit;
               }
-              return sumNumber;
-          },
-          sumOdd(number){
-              let sumNumber = 0;
-              let numLength = number.length;
-              for (let i = 0; i < numLength; i++) {
-                  if (i % 2 === 0) {
-                      sumNumber += parseInt(number[i]);
-                  }
-              }
-              return sumNumber;
+            }
+
+            let X = (O * 2) + (E * 3) + S;
+            let verificationDigit = X % 10;
+
+            return verificationDigit;
           }
       }
   }
